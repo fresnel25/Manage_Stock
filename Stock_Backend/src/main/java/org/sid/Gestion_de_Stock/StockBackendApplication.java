5ef8b3d36a65647076ed35ebdb4626ccd80742f6
fresnel25 <ngaleufresnel@gmail.com>
2023-08-15 16:44:05 +0200

    initialize project

---
+++ Stock_Backend/src/main/java/org/sid/Gestion_de_Stock/StockBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sid.Gestion_de_Stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBackendApplication.class, args);
+	}
+
+}
---
+++ Stock_Backend/src/test/java/org/sid/Gestion_de_Stock/StockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.Gestion_de_Stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
